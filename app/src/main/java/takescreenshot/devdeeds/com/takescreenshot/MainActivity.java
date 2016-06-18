979d282498552c740bb3e319ff3c20204d58bc8b
jaykrishnan PM <jayakrishnan.pm.123@gmail.com>
2016-06-18 20:16:55 +0530

    screencatpure commit
    source code of screen capture programmatically

---
+++ app/src/main/java/takescreenshot/devdeeds/com/takescreenshot/MainActivity.java
@@ -0,0 +1,51 @@
+package takescreenshot.devdeeds.com.takescreenshot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        shareScreen();
+    }
+
+
+
+    private void shareScreen() {
+        try {
+
+
+            File cacheDir = new File(
+                    android.os.Environment.getExternalStorageDirectory(),
+                    "devdeeds");
+
+            if (!cacheDir.exists()) {
+                cacheDir.mkdirs();
+            }
+
+
+            String path = new File(
+                    android.os.Environment.getExternalStorageDirectory(),
+                    "devdeeds") + "/screenshot.jpg";
+
+            Utils.savePic(Utils.takeScreenShot(this), path);
+
+            Toast.makeText(getApplicationContext(), "Screenshot Saved", Toast.LENGTH_SHORT).show();
+
+
+        } catch (NullPointerException ignored) {
+            ignored.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/takescreenshot/devdeeds/com/takescreenshot/Utils.java
@@ -0,0 +1,46 @@
+package takescreenshot.devdeeds.com.takescreenshot;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.view.View;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Utils {
+
+
+    public static Bitmap takeScreenShot(Activity activity) {
+        View view = activity.getWindow().getDecorView();
+        view.setDrawingCacheEnabled(true);
+        view.buildDrawingCache();
+        Bitmap b1 = view.getDrawingCache();
+        Rect frame = new Rect();
+        activity.getWindow().getDecorView().getWindowVisibleDisplayFrame(frame);
+        int statusBarHeight = frame.top;
+
+
+        int width = activity.getWindowManager().getDefaultDisplay().getWidth();
+        int height = activity.getWindowManager().getDefaultDisplay().getHeight();
+
+
+        Bitmap b = Bitmap.createBitmap(b1, 0, statusBarHeight, width, height - statusBarHeight);
+        view.destroyDrawingCache();
+        return b;
+    }
+
+    public static void savePic(Bitmap b, String strFileName) {
+        FileOutputStream fos;
+        try {
+            fos = new FileOutputStream(strFileName);
+            b.compress(Bitmap.CompressFormat.PNG, 90, fos);
+            fos.flush();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
